bdc5f0ea64d5f67c3ca06d25786503fbf17bc70d
Andrew Kostka <apkostka@gmail.com>
2013-09-11 22:08:29 -0700

    init

---
+++ public/style/config.rb
@@ -0,0 +1,11 @@
+# Require any additional compass plugins here.
+require "compass-normalize"
+
+http_path = "/"
+css_dir = "."
+sass_dir = "sass"
+images_dir = "../img"
+javascripts_dir = "../script"
+
+generated_images_dir = images_dir + "/generated"
+relative_assets = true
